73148c48312d7d298892cf12d17982098dfdead8
Mukesh <mukesh.kalojiya55@gmail.com>
2020-05-17 17:22:56 +0530

    remove all files

--- server/middleware/admin.js
+++
@@ -1,9 +0,0 @@
-let admin = (req, res, next) => {
-    if(req.user.role === 0) {
-        return res.send('you are not allowed to create Brand');
-    }
-
-    next();
-}
-
-module.exports = { admin }
